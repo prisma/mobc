6815d4d8a52d62557b1eca8f15d5b3fd1b179f0e
garren smith <garren.smith@gmail.com>
2022-03-22 10:31:15 +0200

    use thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,61 +1,23 @@
-use std::error;
-use std::fmt;
+use thiserror::Error;
 /// The error type returned by methods in this crate.
+#[derive(Error, Debug)]
 pub enum Error<E> {
     /// Manager Errors
+    #[error("{0}")]
     Inner(E),
     /// Timeout
+    #[error("Time out in the connection pool")]
     Timeout,
     /// BadConn
+    #[error("Bad connection in mobc")]
     BadConn,
     /// The pool has been closed or dropped
+    #[error("The pool is closed")]
     PoolClosed,
 }
 
 impl<E> From<E> for Error<E> {
     fn from(e: E) -> Error<E> {
         Error::Inner(e)
-    }
-}
-
-impl<E> fmt::Display for Error<E>
-where
-    E: fmt::Display + 'static,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::Inner(ref err) => write!(f, "{}", err),
-            Error::Timeout => write!(f, "Time out in the connection pool"),
-            Error::BadConn => write!(f, "Bad connection in mobc"),
-            Error::PoolClosed => write!(f, "The pool is closed"),
-        }
-    }
-}
-
-impl<E> fmt::Debug for Error<E>
-where
-    E: fmt::Debug + 'static,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::Inner(ref err) => write!(f, "{:?}", err),
-            Error::Timeout => write!(f, "Timed out in the connection pool"),
-            Error::BadConn => write!(f, "Bad connection in mobc"),
-            Error::PoolClosed => write!(f, "The pool is closed"),
-        }
-    }
-}
-
-impl<E> error::Error for Error<E>
-where
-    E: error::Error + 'static,
-{
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match *self {
-            Error::Inner(ref err) => Some(err),
-            Error::Timeout => None,
-            Error::BadConn => None,
-            Error::PoolClosed => None,
-        }
     }
 }
